d1f1dedd621f6b5c1bcb6cf881f7a2a768e2129f
JeremyPK <jpkeeton@gmail.com>
2020-07-18 13:21:49 -0700

    Adding my tkinter files

---
+++ antherTkinterTry.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import *
+
+##Import of Tkinter module
+
+window = tk.Tk()
+#Creates the window from the imported Tkinter module
+window.geometry("600x400")
+
+##Creates the size of the window
+
+window.title("Software Testing Quiz")
+##Adds a title to the Windows GUI for the window
+
+greeting = tk.Label(text="Hello, Tkinter")
+
+window.mainloop()
+##Loops the window to prevent the window from just "flashing once"
---
+++ tkinterTutorial.py
@@ -0,0 +1,13 @@
+# https://www.youtube.com/watch?v=YXPyB4XeYLA
+
+from tkinter import *
+
+root = Tk()
+
+# create a label widget
+myLabel = Label(root, text="Software Testing Quiz")
+
+myLabel.pack()
+
+root.mainloop()
+
